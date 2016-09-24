b3ad6aa1a8de75cb4eb5cf292c14299652147d3c
Diego F. Ruiz S <yego23@gmail.com>
2016-09-23 19:43:17 -0500

    Iteración 2 - Ciclo 4 - (Verde)

--- Procesador.py
+++ Procesador.py
@@ -2,9 +2,14 @@ class Procesador:
     def procesar(self, lista):
 
         if lista == "":
+            '''Cuando viene vacía la lista se retorna el arreglo con [0,0]'''
             return [0, 0]
         else:
             '''Recibo la lista y hago un split para eliminar las ',' entonces,
             utilizo len para saber el número de datos'''
             datos = str.split(lista, ',')
-            return {len(datos)}
+            '''Ahora se necesita retornar el mínimo, más el arreglo.
+            Utilizamos la variable auxiliar para guardar el nuevo arreglo datos'''
+            auxiliar = [int(i) for i in datos]
+            '''Utilizamos la nueva funcion min para hallar el mínimo'''
+            return [len(auxiliar), min(auxiliar)]
