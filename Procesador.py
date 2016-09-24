2300075dccdae6399425150a39aec20cf48b1c14
Diego F. Ruiz S <yego23@gmail.com>
2016-09-23 19:12:47 -0500

    Iteración 1 - Ciclo 4 - (Verde)

--- Procesador.py
+++ Procesador.py
@@ -1,3 +1,7 @@
 class Procesador:
-    def procesar(self,lista):
-        return 0
+    def procesar(self, lista):
+
+        if lista == "":
+            return 0
+        else:
+            return 1
