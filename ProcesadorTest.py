9fa125e31a21a6119d9799d4684dc54d7c802a38
Diego F. Ruiz S <yego23@gmail.com>
2016-09-23 19:09:53 -0500

    Iteración 1 - Ciclo 3 - (Rojo)

--- ProcesadorTest.py
+++ ProcesadorTest.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
 
 from Procesador import *
+
+
 class ProcesadorTest(TestCase):
     def test_procesador(self):
         self.assertEquals(Procesador().procesar(""), 0, "Lista sin datos")
+
+    def test_procesador_1(self):
+        self.assertEquals(Procesador().procesar("1"), 1, "Lista con 1 dato")
