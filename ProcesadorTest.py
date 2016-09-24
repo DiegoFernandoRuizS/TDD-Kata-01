5686cf9c881e4e96b3a20e844bab6a4e9fb9550d
Diego F. Ruiz S <yego23@gmail.com>
2016-09-23 19:44:50 -0500

    Iteración 3 - Ciclo 1 - (Rojo)

--- ProcesadorTest.py
+++ ProcesadorTest.py
@@ -5,10 +5,10 @@ from Procesador import *
 
 class ProcesadorTest(TestCase):
     def test_procesador(self):
-        self.assertEquals(Procesador().procesar(""), [0,0], "Lista sin datos")
+        self.assertEquals(Procesador().procesar(""), [0,0,0], "Lista sin datos")
 
     def test_procesador_1(self):
-        self.assertEquals(Procesador().procesar("2"), [1,2], "Lista con 1 dato")
+        self.assertEquals(Procesador().procesar("2"), [1,2,4], "Lista con 1 dato")
 
     def test_procesador_2(self):
         self.assertEquals(Procesador().procesar("3,0"), [2,0], "Lista con 2 datos")
