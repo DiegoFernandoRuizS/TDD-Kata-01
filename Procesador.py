5228d244a3d197354c8dbc83f7e3c651f2098164
Diego F. Ruiz S <yego23@gmail.com>
2016-09-23 19:29:15 -0500

    Iteración 2 - Ciclo 2 - (Verde)

--- Procesador.py
+++ Procesador.py
@@ -2,7 +2,7 @@ class Procesador:
     def procesar(self, lista):
 
         if lista == "":
-            return {0}
+            return [0, 0]
         else:
             '''Recibo la lista y hago un split para eliminar las ',' entonces,
             utilizo len para saber el número de datos'''
