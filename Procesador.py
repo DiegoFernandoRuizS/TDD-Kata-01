4e0d948dfbf2700b31858431a16f82df494c727f
Diego F. Ruiz S <yego23@gmail.com>
2016-09-23 19:50:19 -0500

    Iteración 3 - Ciclo 4 - (Verde)

--- Procesador.py
+++ Procesador.py
@@ -11,5 +11,5 @@ class Procesador:
             '''Ahora se necesita retornar el mínimo, más el arreglo.
             Utilizamos la variable auxiliar para guardar el nuevo arreglo datos'''
             auxiliar = [int(i) for i in datos]
-            '''Utilizamos la nueva funcion min para hallar el mínimo'''
-            return [len(auxiliar), min(auxiliar)]
+            '''Utilizamos la nueva funcion min para hallar el mínimo, usamos max para el máximo'''
+            return [len(auxiliar), min(auxiliar), max(auxiliar)]
